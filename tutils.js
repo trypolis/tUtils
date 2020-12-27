050a756e7f473dce90742a7f5ec150e6f43730e4
Ty <turret1226@gmail.com>
2020-12-27 15:59:07 -0700

    Added generateToken function

--- tutils.js
+++ tutils.js
@@ -12,3 +12,14 @@
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Generates a string of random characters.
+function generateToken(length) {
+  // Feel free to edit as needed depending on what you want generated.
+  let symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  let final = '';
+  for (let i = 0; i < length; i++) {
+    final += symbols[randomInt(0, symbols.length - 1)];
+  }
+  return final;
+}
