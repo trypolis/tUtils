9da7e9b93d03449fe80319436271d6c44a54382c
Ty <turret1226@gmail.com>
2020-12-27 20:03:22 -0700

    Double quotes for the win!

--- tutils.js
+++ tutils.js
@@ -6,17 +6,17 @@
  * MIT License.
  */
 
-'use strict';
+"use strict";
 
-// JavaScript doesn't provide an easy way to get a random int out of the box, because reasons.
+// JavaScript doesn"t provide an easy way to get a random int out of the box, because reasons.
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function generateToken(length) {
   // Feel free to edit as needed depending on what you want generated.
-  let symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-  let final = '';
+  let symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  let final = "";
   for (let i = 0; i < length; i++) {
     final += symbols[randomInt(0, symbols.length - 1)];
   }
@@ -24,29 +24,29 @@ function generateToken(length) {
 }
 
 function formatTime() {
-  let clockString = '';
+  let clockString = "";
   let currentTime = new Date();
   let hours = currentTime.getHours();
   let minutes = currentTime.getMinutes();
   if (minutes < 10) {
-    minutes = '0' + minutes;
+    minutes = "0" + minutes;
   }
   if (hours > 12) {
     // We want 12-hour time.
     hours -= 12;
-    clockString += hours + ':' + minutes + ' PM';
+    clockString += hours + ":" + minutes + " PM";
   } else {
-    clockString += hours + ':' + minutes + ' AM';
+    clockString += hours + ":" + minutes + " AM";
   }
   return clockString;
 }
 
 function formatDate() {
-  let clockString = '';
+  let clockString = "";
   let today = new Date();
-  let dd = String(today.getDate()).padStart(2, '0');
-  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   let yyyy = today.getFullYear();
-  clockString += '' + mm + '/' + dd + '/' + yyyy;
+  clockString += "" + mm + "/" + dd + "/" + yyyy;
   return clockString;
 }
