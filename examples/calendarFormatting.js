7eaf27715ef2d619cd02c9ad5b81b9a76df391f9
Ty <turret1226@gmail.com>
2020-12-30 20:58:23 -0700

    Calendar formatting example is done
    formatTime and formatDate have an example in the same file (for now at least).

---
+++ examples/calendarFormatting.js
@@ -0,0 +1,11 @@
+/*
+ * tUtils.
+ * JavaScript library of lots of functions.
+ * 
+ * Copyright (C) 2020, Ty Gillespie. All rights reserved.
+ * MIT License.
+ */
+
+'use strict';
+
+document.getElementById("the-calendar").innerHTML = formatTime() + ", on " + formatDate();
